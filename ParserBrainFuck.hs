4edfadbf3330409f27f22b24efe932ccd8db4052
jotaalvim <joaoafonsoalvim@gmail.com>
2024-09-23 16:01:40 +0200

    readability

--- ParserBrainFuck.hs
+++ ParserBrainFuck.hs
@@ -2,31 +2,25 @@ module ParserBrainFuck where
 
 import Parser
 import Types
+
 ----------- PARSER BrainFuck --------------------------------------------------
 
-parserAdd,parserSub,parserInc,parserDec,parserOut,parserInp :: Parser Inst
+parserAdd,parserSub,parserInc,parserDec,parserOut,parserInp,parserLoop,parserInst,parserCmt :: Parser Inst
+
 parserAdd = const Add <$$> symbol' '+'
-
 parserSub = const Sub <$$> symbol' '-'
-
 parserInc = const Inc <$$> symbol' '>'
-
 parserDec = const Dec <$$> symbol' '<'
-
 parserOut = const Out <$$> symbol' '.'
-
 parserInp = const Inp <$$> symbol' ',' 
 
-parserLoop :: Parser Inst
-parserLoop = f <$$> (symbol' '[') <**> parserMultInst <**> symbol' ']'
-    where f _ b _ = Loop b
-
 isComment = not . flip elem "+-><-,[]"
-
-parserCmt :: Parser Inst
 parserCmt = Cmt <$$> oneOrMore ( satisfy' isComment )
 
-parserInst :: Parser Inst
+parserLoop = f <$$> symbol' '[' <**> parserMultInst <**> symbol' ']'
+    where f _ b _ = Loop b
+
+
 parserInst = parserAdd 
          <|> parserSub
          <|> parserInc
@@ -39,5 +33,6 @@ parserInst = parserAdd
 parserMultInst :: Parser BrainFuck 
 parserMultInst = zeroOrMore parserInst
     
+parserBrainFuck :: String -> BrainFuck
 parserBrainFuck = fst . head . filter (null . snd) . parserMultInst
 
